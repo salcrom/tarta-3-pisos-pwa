636be53974159bcf05031fcea40db86834429a50
Sergio Alcántara Romero <seya.28@gmail.com>
2026-08-19 12:59:17 +0200

    Actualiza caché PWA a v6

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='tarta-3-pisos-v5';
+const CACHE_NAME='tarta-3-pisos-v6';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));self.clients.claim();});
